27cb582e686311cadb4a62d683fde695172aaf66
Haruka x Music 🎶 <deepakkumar54dk16@gmail.com>
2025-09-02 16:50:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram API
-API_ID = int(os.getenv("API_ID", 6435225))  
-API_HASH = os.getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+API_ID = int(os.getenv("20071630")) 
+API_HASH = os.getenv("11d0d49035cfa33964c687e4480fef0d")
 
 # Bot Token / String Session
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
@@ -15,9 +15,9 @@ STRING1 = os.getenv("STRING_SESSION", None)
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://pusers:nycreation@nycreation.pd4klp1.mongodb.net/?retryWrites=true&w=majority&appName=NYCREATION")
 
 # Owner / Admin
-OWNER_ID = int(os.getenv("OWNER_ID", 7574330905))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "WTF_WhyMeeh")
+OWNER_ID = int(os.getenv("8306720320"))
+OWNER_USERNAME = os.getenv("OWNER_USERNAME")
 
 # Support / Updates
-SUPPORT_GRP = os.getenv("SUPPORT_GRP", "ShrutiBotSupport")
-UPDATE_CHNL = os.getenv("UPDATE_CHNL", "ShrutiBots")
+SUPPORT_GRP = os.getenv("SUPPORT_GRP", "https://t.me/JAWANON_KI_TOLI")
+UPDATE_CHNL = os.getenv("UPDATE_CHNL", "https://t.me/JAWANON_KA_ADDA")
