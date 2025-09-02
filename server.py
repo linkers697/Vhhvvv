bfe232e88a52682f294831efc1ec9a91639c3ec7
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-09-02 21:37:37 +0530

    Update server.py to server.py

--- server.py
+++ server.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "✅ Shruti ChatBot is alive on Render!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
